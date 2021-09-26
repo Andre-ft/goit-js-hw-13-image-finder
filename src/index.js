47e60fa6d64d29e01c33e73bae9a93ae699b555f
Andrey F <media.andrej@gmail.com>
2021-09-26 20:05:10 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import css from './styles.css';
+import apiService from './js/apiService';
+
+
+
+
+const API_KEY = '23458770-c93bd78e83fb2002196f13d31';
---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+const webpack = require("webpack");
+
+const path = require("path");
+const { merge } = require("webpack-merge");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const FriendlyErrorsWebpackPlugin = require("friendly-errors-webpack-plugin");
+const WebpackBar = require("webpackbar");
+
+const ASSET_PATH = process.env.ASSET_PATH || '/goit-js-hw-13-image-finder/';
+// /goit-js-hw-13-image-finder/
+const loadModeConfig = (env) =>
+  require(`./build-utils/${env.mode}.config.js`)(env);
+
+module.exports = (env) =>
+  merge(
+    {
+      mode: env.mode,
+      context: path.resolve(__dirname, "src"),
+
+      entry: "./index.js",
+      output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "[name].bundle.js",
+        publicPath: ASSET_PATH,
+      },
+      module: {
+        rules: [
+          {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: ["babel-loader"],
+          },
+          {
+            test: /\.html$/,
+            use: ["html-loader"],
+          },
+          {
+            test: /\.(gif|png|jpe?g|svg)$/,
+            use: [
+              {
+                loader: "url-loader",
+                options: {
+                  name: "[path]/[name].[ext]",
+                  limit: false,
+                },
+              },
+            ],
+          },
+          {
+            test: /\.hbs$/,
+            exclude: /node_modules/,
+            use: ["handlebars-loader"],
+          },
+        ],
+      },
+      plugins: [
+        new CleanWebpackPlugin(),
+        new FriendlyErrorsWebpackPlugin(),
+        new WebpackBar(),
+        new webpack.DefinePlugin({
+          'process.env.ASSET_PATH': JSON.stringify(ASSET_PATH),
+        }),
+      ],
+    },
+    loadModeConfig(env),
+  );
